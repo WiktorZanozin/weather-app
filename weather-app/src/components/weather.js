9b12c73afe4e98e395652d2a6a3297261ea472df
Viktor Zanozin <wiktor.zanozin@gmail.com>
2020-07-07 22:41:30 +0200

    test2

--- weather-app/src/components/weather.js
+++ weather-app/src/components/weather.js
@@ -2,11 +2,11 @@ import React from "react";
 
 const Weather =(props)=>(
         <div className="weather__info">
-        <p className="weather__key">Location: <span className="weather__value">{props.city} {props.country}</span></p>
-        <p className="weather__key">Temperature:<span className="weather__value">{props.temperature}</span> </p>
-        <p className="weather__key">Humidity:<span className="weather__value">{props.humidity}</span></p>
-        <p className="weather__key">Condition:<span className="weather__value">{props.description}</span></p>
-        <p className="weather__key">{props.error}</p>
+                <p className="weather__key">Location: <span className="weather__value">{props.city} {props.country}</span></p>
+                <p className="weather__key">Temperature:<span className="weather__value">{props.temperature}</span> </p>
+                 <p className="weather__key">Humidity:<span className="weather__value">{props.humidity}</span></p>
+                <p className="weather__key">Condition:<span className="weather__value">{props.description}</span></p>
+                 <p className="weather__key">{props.error}</p>
         </div>
 );
 export default Weather;
